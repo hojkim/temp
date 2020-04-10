ac39b057df131e871f0a7e3cf851e4b412f6c71d
hojkim <hojunkim2@gmail.com>
2020-04-09 20:29:32 -0700

    Added revisions to Assignment 01

--- app/src/main/java/com/hkim/dotify/MainActivity.kt
+++ app/src/main/java/com/hkim/dotify/MainActivity.kt
@@ -2,11 +2,33 @@ package com.hkim.dotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private var playCount = Random.nextInt(0, 100000)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvPlayCount = findViewById<TextView>(R.id.tvPlayCount)
+        tvPlayCount.text = "$playCount plays"
+    }
+
+    fun playClicked (view: View) {
+        playCount++
+        val tvPlayCount = findViewById<TextView>(R.id.tvPlayCount)
+        tvPlayCount.text = "$playCount plays"
+    }
+
+    fun previousClicked (view: View) {
+        Toast.makeText(this, "Skipping to previous track", Toast.LENGTH_SHORT).show()
+    }
+
+    fun nextClicked (view: View) {
+        Toast.makeText(this, "Skipping to next track", Toast.LENGTH_SHORT).show()
     }
 }
